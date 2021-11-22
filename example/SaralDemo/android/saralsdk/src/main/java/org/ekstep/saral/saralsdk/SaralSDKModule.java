7aaf7b03cc9d9fa225803b864ae56823c7d54743
KD <kumar.deepak@tarento.com>
2021-11-22 11:40:42 +0530

    develop-debug

--- example/SaralDemo/android/saralsdk/src/main/java/org/ekstep/saral/saralsdk/SaralSDKModule.java
+++ example/SaralDemo/android/saralsdk/src/main/java/org/ekstep/saral/saralsdk/SaralSDKModule.java
@@ -2,6 +2,9 @@ package org.ekstep.saral.saralsdk;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
@@ -15,11 +18,17 @@ import com.facebook.react.bridge.ReactMethod;
 import org.ekstep.saral.saralsdk.commons.FileOps;
 import org.ekstep.saral.saralsdk.hwmodel.HWClassifier;
 import org.ekstep.saral.saralsdk.hwmodel.HWClassifierStatusListener;
+import org.ekstep.saral.saralsdk.hwmodel.PredictionListener;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SaralSDKModule extends ReactContextBaseJavaModule implements ActivityEventListener {
     private static final String TAG             = "SrlSDK::Module";
@@ -61,6 +70,7 @@ public class SaralSDKModule extends ReactContextBaseJavaModule implements Activi
             @Override
             public void OnModelLoadSuccess(String message) {
                 Log.d(TAG, "HWClassifer model loaded : " + message);
+                doLocalClassification(context);
             }
 
             @Override
@@ -109,5 +119,32 @@ public class SaralSDKModule extends ReactContextBaseJavaModule implements Activi
     @Override
     public void onNewIntent(Intent intent) {
         Log.d(TAG, "SrlSDK:: onNewIntent");
+    }
+
+    private void doLocalClassification(ReactApplicationContext context) {
+
+        HWClassifier.getInstance().setPredictionListener(new PredictionListener() {
+            @Override
+            public void OnPredictionSuccess(int digit, float confidence, String id) {
+                Log.d(TAG, "predicted digit:" + digit + " unique id:" + id + " confidence:" + confidence);
+            }
+
+            @Override
+            public void OnPredictionFailed(String error, String id) {
+                Log.e(TAG, "Model prediction failed for id: " + id);
+            }
+        });
+
+        AssetManager assetManager = context.getAssets();
+        try {
+            InputStream is = assetManager.open("site-digits/gj_dig_1_04.jpg");
+            Bitmap  bitmap = BitmapFactory.decodeStream(is);
+            Mat digitROI   = new Mat();
+            Utils.bitmapToMat(bitmap, digitROI);
+            HWClassifier.getInstance().classifyMat(digitROI, "digitROI");
+
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
 }
